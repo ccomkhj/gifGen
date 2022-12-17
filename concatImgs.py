43e3368ac371c14d25ac233e3f0b5eab40a595fc
huijo <ccomkhj@gmail.com>
2022-12-17 15:04:17 +0100

    add examples

--- concatImgs.py
+++ concatImgs.py
@@ -5,14 +5,8 @@ import os
 import click
 import cv2
 
-def concat_tile(im1, im2, im3, im4):
-    img1 = cv2.imread(im1)
-    img2 = cv2.imread(im2)
-    img3 = cv2.imread(im3)
-    img4 = cv2.imread(im4)
-    return cv2.resize(cv2.vconcat([cv2.hconcat([img1, img2]), cv2.hconcat([img3, img4])]), (img1.shape[1], img1.shape[0]))
-
-
+def concat_tile(im_list_2d):
+    return cv2.vconcat([cv2.hconcat(im_list_h) for im_list_h in im_list_2d])
 
 @click.command()
 @click.argument('in_dir', type=str)
